79f2f9e7b510af4324460bcc2870e924ac8fed54
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-28 09:47:51 +0000

    test

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,1 +1,16 @@
-puts 'Hello, world!'
+def fizz_buzz(n)
+  if n % 15 ==0 then 'Fizz Buzz'
+  elsif n % 3 == 0 then 'Fizz'
+  elsif n % 5 == 0 then 'Buzz'
+  else 
+    n.to_s
+  end 
+end 
+
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(4)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(15)
