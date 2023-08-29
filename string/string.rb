89f652b05b40c4ff9555c52111919b8a143e93bf
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-29 14:26:41 +0000

    add

--- string/string.rb
+++ string/string.rb
@@ -1,4 +1,14 @@
 puts 'abc'.class
 
 first_name, _ = "Scott Tiger".split(' ')
-puts first_name
+puts first_name.upcase
+
+puts "許して\nください"
+
+name = 'Alice'
+puts "Hello, #{name}!"
+
+n = 1
+puts n -= 1
+
+puts %q!He said, "Don't speak."!
