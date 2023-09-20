c99a5a62e410d2b84d641869b4f668160f9b33ad
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-09-20 12:51:55 +0000

    add

--- array.rb
+++ array.rb
@@ -19,3 +19,31 @@ b = Set[3, 4, 5]
 puts a | b
 puts a - b
 puts a & b
+
+e, *f = 100, 200, 300
+# puts e
+puts f
+
+a, *b, c, d = 1, 2, 3, 4, 5
+puts b
+
+a = []
+b = [2, 3]
+a.push(1)
+a.push(b)
+puts a
+
+jp = ['japan', '日本']
+country = '日本'
+case country
+when *jp
+  puts 'こんにちは'
+end
+
+def greet(*names)
+  "#{names.join('と')}, こんにちは！"
+end 
+
+puts greet('田中さん')
+puts greet('田中さん', '鈴木さん')
+puts greet('田中さん', '鈴木さん', '佐藤さん')
