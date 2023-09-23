efef024e7777a21c124afe5311a916091520e725
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-09-23 13:03:03 +0000

    add

--- with_index.rb
+++ with_index.rb
@@ -1,2 +1,16 @@
 fruits = ['apple', 'oranege', 'melon']
 fruits.each_with_index { |fruit, i| puts "#{i}: #{fruit}" }
+
+fruits = ['apple', 'oranege', 'melon']
+fruits.map.with_index { |fruit, i| puts "#{i}: #{fruit}" }
+puts fruits
+
+fruits = ['apple', 'oranege', 'melon']
+fruits.delete_if.with_index { |fruit, i| fruit.include?('a') && i.odd? }
+puts fruits
+
+fruits = ['apple', 'orange', 'melon']
+p fruits.each
+
+fruits = ['apple', 'oranege', 'melon']
+fruits.each.with_index(1) { |fruit, i| puts "#{i}: #{fruit}" }
