4a8e13663c87d4d353f665023596baf541d1ef72
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-10-01 12:57:28 +0000

    add

---
+++ break.rb
@@ -0,0 +1,28 @@
+numbers = [1, 2, 3, 4, 5].shuffle 
+numbers.each do |n|
+  puts n
+  break if n == 5 
+end
+
+i = 0 
+while i < numbers.size 
+  n = numbers[i] 
+  puts n 
+  break if n == 5 
+  i += 1 
+end 
+
+ret =
+  while true 
+    break 123 
+  end 
+puts ret
+
+fruits = ['apple', 'melon', 'orange']
+numbers = [1, 2, 3]
+fruits.each do |fruit|
+  numbers.shuffle.each do |n| 
+    puts "#{fruit}, #{n}"
+    break if n == 3 
+  end 
+end 
