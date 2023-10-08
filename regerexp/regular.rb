7eceb8e2cb1b849ea2f250e487a1cf51713a71e6
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-10-08 13:29:22 +0000

    add

--- regerexp/regular.rb
+++ regerexp/regular.rb
@@ -1,9 +1,27 @@
-old_syntax = <<TEXT
-{
-  :name => 'Alice',
-  :age=>20,
-  :gender  =>. :female
-}
+# old_syntax = <<TEXT
+# {
+#   :name => 'Alice',
+#   :age=>20,
+#   :gender  =>. :female
+# }
+# TEXT
+
+# convert_hash_syntax(old_syntax)
+
+text = <<-TEXT
+名前：伊藤淳一
+電話：03-1234-5678
+住所：兵庫県西脇市板波町1-2-3
+TEXT
+puts text.scan /\d\d-\d\d\d\d-\d\d\d\d/
+
+text = <<-TEXT
+クープバゲットのパンは美味しかった。
+今日はクープ バゲットさんに行きました。
+クープ　バゲットのパンは最高。
+ジャムおじさんのパン、ジャムが入ってた。
+また行きたいです。クープ・バゲット。
+クープ・バケットのパン、売り切れだった（><）
 TEXT
 
-convert_hash_syntax(old_syntax)
+puts text.split(/\n/).grep(/クープ.?バ[ゲケ]ット/)
