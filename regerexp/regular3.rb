270374a3b181181ddf1dba4fc1fbe938098bfb35
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-10-12 07:01:31 +0000

    add

--- regerexp/regular3.rb
+++ regerexp/regular3.rb
@@ -10,3 +10,20 @@ puts m[1]
 puts m[2]
 puts m[3]
 puts m[1..3]
+
+text = '私の誕生日は1977年7月17日です'
+m = /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/.match(text)
+puts m[:year]
+puts m[:month]
+puts m[:day]
+puts m['year']
+
+text = '私の誕生日は1977年7月17日です'
+if /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/ =~ text
+  puts "#{year}/#{month}/#{day}"
+end 
+
+text = '私の誕生日は1977年7月17日です'
+text =~ /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/
+$~
+$&
